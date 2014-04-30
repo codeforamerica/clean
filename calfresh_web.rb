3597514fccc08bc44a793c7eceb56c4de78ca7a5
Jake Solomon <jacob@codeforamerica.org>
2014-04-30 08:44:29 -0700

    basic bootstrap form styles

--- calfresh_web.rb
+++ calfresh_web.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
 
-class CalfreshWeb < Sinatra::Base
-  get '/' do
-    erb :index
-  end
-
-  post '/applications' do
-    erb :application_sent
-  end
+# class CalfreshWeb < Sinatra::Base
+get '/' do
+  erb :index
 end
+
+post '/applications' do
+  erb :application_sent
+end
+# end
