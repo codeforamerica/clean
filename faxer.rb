f4acd1a496d70c7db89c4697763fc8f68d4cc4fd
Dave Guarino <dave@codeforamerica.org>
2014-04-29 20:06:32 -0400

    Fix range syntax in faxer

--- faxer.rb
+++ faxer.rb
@@ -11,7 +11,7 @@ module Faxer
   def fax_calfresh_pngs_from_tmp(unique_key)
     file_array = Array.new
     file_array << File.new("/tmp/application_#{unique_key}-6-signed.png")
-    [7..15].each do |page_number|
+    (7..15).each do |page_number|
       file_array << File.new("/tmp/application_#{unique_key}-#{page_number}.png")
     end
     result = Phaxio.send_fax(to: ENV['FAX_DESTINATION_NUMBER'], filename: file_array)
