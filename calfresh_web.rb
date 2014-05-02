5086c9e932c66b1225cf59bfa9c5b642feb65be1
Dave Guarino <dave@codeforamerica.org>
2014-05-02 01:45:58 +0000

    If no fax number configured, tell user and give link to application PNG; otherwise send fax (mocked for now)

--- calfresh_web.rb
+++ calfresh_web.rb
@@ -7,10 +7,21 @@ class CalfreshWeb < Sinatra::Base
   end
 
   post '/applications' do
-    puts params
     writer = Calfresh::ApplicationWriter.new
-    application = writer.fill_out_form(params)
-    send_file application.signed_png_path
-    erb :application_sent
+    @application = writer.fill_out_form(params)
+    if ENV.has_key?('FAX_DESTINATION_NUMBER') && ENV['FAX_DESTINATION_NUMBER'] != ""
+      if @application.has_pngs?
+        puts "fax mock"
+        # Faxer.send_fax(ENV['FAX_DESTINATION_NUMBER'], @application.png_file_set)
+        erb :sent
+      end
+    else
+      puts "no fax number!"
+      erb :no_fax
+    end
+  end
+
+  get '/applications/:id' do
+    send_file Calfresh::Application.new(params[:id]).signed_png_path
   end
 end
