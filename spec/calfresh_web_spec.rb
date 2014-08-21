4449d3cec17fccf511b0e23ff361208c3ba76b28
Dave Guarino <dave@codeforamerica.org>
2014-08-21 10:28:27 -0700

    Test for POST contact_info

--- spec/calfresh_web_spec.rb
+++ spec/calfresh_web_spec.rb
@@ -10,10 +10,40 @@ describe CalfreshWeb do
   end
 
   describe 'POST /application/basic_info' do
-    it 'asdasd' do
+    it 'saves basic_info to the session' do
       input_hash = { name: 'dave', date_of_birth: '06/01/75' }
       post '/application/basic_info', input_hash
       expect(last_request.session).to eq(input_hash)
+    end
+  end
+
+  describe 'GET /application/contact_info' do
+    it 'responds successfully' do
+      get '/application/contact_info'
+      expect(last_response.status).to eq(200)
+    end
+  end
+
+  describe 'POST /application/contact_info' do
+    before do
+      @input_hash = {
+        home_phone_number: '1112223333',
+        email: 'joe@example.com',
+        home_address: '1234 Fake St',
+        home_zip_code: '94113',
+        home_city: 'San Francisco',
+        home_state: 'CA'
+      }
+      post '/application/contact_info', @input_hash
+    end
+
+    it 'saves contact info into the session' do
+      expect(last_request.session).to eq(@input_hash)
+    end
+
+    it 'redirects to sex_and_ssn page' do
+      expect(last_response).to be_redirect
+      expect(last_response.location).to include('/application/sex_and_ssn')
     end
   end
 end
