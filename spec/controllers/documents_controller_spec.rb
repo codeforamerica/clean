e5de4432f66b8a7989b4a51f09988bf1dcff51e8
Dave Guarino <dave@codeforamerica.org>
2015-02-11 09:57:20 -0800

    Add ~20 test examples for document submission

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe DocumentsController, :type => :controller do
+  around do |example|
+    ClimateControl.modify(REDISTOGO_URL: 'fakeredisurl',
+                          ZIP_FILE_PASSWORD: 'fakezipfilepassword',
+                          SENDGRID_USERNAME: 'fakesendgridusername',
+                          SENDGRID_PASSWORD: 'fakesendgridpassword',
+                          EMAIL_ADDRESS_TO_SEND_TO: 'emailto@sendto.com') do
+      example.run
+    end
+  end
+
   describe 'GET new' do
     it 'is successful' do
       get :new, { :user_token => 'faketoken', :number_of_docs => '0' }
@@ -46,26 +56,115 @@ RSpec.describe DocumentsController, :type => :controller do
   end
 
   describe 'POST /documents/USERTOKEN/DOCNUMBER/submit' do
-    let(:fake_redis) { double("Redis", :set => nil, :expire => nil) }
-    let(:fake_sendgrid_client) { double("SendGrid::Client") }
-    let(:fake_sendgrid_mail) { double("SendGrid::Mail") }
-    let(:fake_file) { double("File") }
+    let(:fake_redis) { double("Redis") }
+    let(:fake_sendgrid_client) { double("SendGrid::Client", :send => "message: success") }
+    let(:fake_sendgrid_mail) { double("SendGrid::Mail", :add_attachment => true) }
+    let(:fake_file_for_block) { double("", :write => true) }
+    let(:fake_zip_archive_for_block) { double("", :add_file => true) }
 
     before do
       allow(Redis).to receive(:new).and_return(fake_redis)
+      allow(fake_redis).to receive(:get).with("fakeusertoken_0_binary").and_return("fakebinary0")
+      allow(fake_redis).to receive(:get).with("fakeusertoken_1_binary").and_return("fakebinary1")
+      allow(fake_redis).to receive(:get).with("fakeusertoken_0_filename").and_return("fakefilename0")
+      allow(fake_redis).to receive(:get).with("fakeusertoken_1_filename").and_return("fakefilename1")
+      allow(fake_redis).to receive(:del).with("fakeusertoken_0_binary")
+      allow(fake_redis).to receive(:del).with("fakeusertoken_1_binary")
+      allow(fake_redis).to receive(:del).with("fakeusertoken_0_filename")
+      allow(fake_redis).to receive(:del).with("fakeusertoken_1_filename")
       allow(SendGrid::Client).to receive(:new).and_return(fake_sendgrid_client)
       allow(SendGrid::Mail).to receive(:new).and_return(fake_sendgrid_mail)
-      # PENDING
-      #post :submit, { 'user_token' => 'fakeusertoken', 'doc_number' => '1' }
+      allow(File).to receive(:open).and_yield(fake_file_for_block)
+      allow(Kernel).to receive(:system)
+      allow(Zip::Archive).to receive(:open).and_yield(fake_zip_archive_for_block)
+      allow(Zip::Archive).to receive(:encrypt)
+      post :submit, { 'user_token' => 'fakeusertoken', 'doc_number' => '2' }
     end
 
     context 'with two verification documents' do
       it 'instantiates a redis client' do
-        pending
         expect(Redis).to have_received(:new)
       end
 
+      it 'stubs the ENV correctly' do
+        expect(ENV['REDISTOGO_URL']).to eq('fakeredisurl')
+        expect(ENV['ZIP_FILE_PASSWORD']).to eq('fakezipfilepassword')
+      end
+
+      it 'sends the correct :get arguments to the Redis client' do
+        expect(fake_redis).to have_received(:get).with("fakeusertoken_0_binary")
+        expect(fake_redis).to have_received(:get).with("fakeusertoken_1_binary")
+        expect(fake_redis).to have_received(:get).with("fakeusertoken_0_filename")
+        expect(fake_redis).to have_received(:get).with("fakeusertoken_1_filename")
+      end
+
+      it 'sends the correct :del arguments to the Redis client' do
+        expect(fake_redis).to have_received(:del).with("fakeusertoken_0_binary")
+        expect(fake_redis).to have_received(:del).with("fakeusertoken_1_binary")
+        expect(fake_redis).to have_received(:del).with("fakeusertoken_0_filename")
+        expect(fake_redis).to have_received(:del).with("fakeusertoken_1_filename")
+      end
+
+      it 'writes files with the correct name' do
+        expect(File).to have_received(:open).with('/tmp/fakeusertokenfakefilename0', 'wb')
+        expect(fake_file_for_block).to have_received(:write).with("fakebinary0")
+        expect(File).to have_received(:open).with('/tmp/fakeusertokenfakefilename1', 'wb')
+        expect(fake_file_for_block).to have_received(:write).with("fakebinary1")
+      end
+
+      it 'shells out to convert with the correct arguments' do
+        convert_statement = "convert /tmp/fakeusertokenfakefilename0 /tmp/fakeusertokenfakefilename1 /tmp/fakeusertoken_all_images.pdf"
+        expect(Kernel).to have_received(:system).with(convert_statement)
+      end
+
+      it 'zips the combined-image file' do
+        expect(Zip::Archive).to have_received(:open).with('/tmp/fakeusertoken_zipped.zip', Zip::CREATE)
+        expect(fake_zip_archive_for_block).to have_received(:add_file).with('/tmp/fakeusertoken_all_images.pdf')
+      end
+
+      it 'encrypts the zip file' do
+        expect(Zip::Archive).to have_received(:encrypt).with('/tmp/fakeusertoken_zipped.zip', 'fakezipfilepassword')
+      end
+
+      it 'creates a new SendGrid client with credentials from env' do
+        expect(SendGrid::Client).to have_received(:new).with({ api_user: 'fakesendgridusername', api_key: 'fakesendgridpassword' })
+      end
+
+      it 'prepares an email with correct inputs' do
+        email_body = <<EOF
+Hi there!
+
+Verification docs were just submitted for a CalFresh application!
+
+You can find the docs in the attached .zip file.
+
+The .zip file attached is encrypted because it contains sensitive personal information. If you don't have a key to access it, please get in touch with Jake Soloman at jacob@codeforamerica.org
+
+Thanks for your time!
+
+Suzanne, your friendly neighborhood CalFresh robot
+EOF
+        expect(SendGrid::Mail).to have_received(:new).with(
+          to: 'emailto@sendto.com',
+          from: 'suzanne@cleanassist.org',
+          subject: 'New Clean CalFresh Verification Docs!',
+          text: email_body
+        )
+      end
+
       # More tests
+
+      it 'adds attachment with correct path to the mail' do
+        expect(fake_sendgrid_mail).to have_received(:add_attachment).with('/tmp/fakeusertoken_zipped.zip')
+      end
+
+      it 'sends the mail' do
+        expect(fake_sendgrid_client).to have_received(:send).with(fake_sendgrid_mail)
+      end
+
+      pending
+      it 'redirects to /complete' do
+      end
     end
   end
 end
