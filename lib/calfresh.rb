b407c8efcc6eea10d813a183f6a5b4c13cce77a6
Dave Guarino <dave@codeforamerica.org>
2015-02-25 13:00:12 -0800

    Replace cover letter to have Jake as contact, closes #378

--- lib/calfresh.rb
+++ lib/calfresh.rb
@@ -72,7 +72,7 @@ module Calfresh
       stamped_app_without_cover_letter_path = "/tmp/final_application_no_cover_letter_#{unique_key}.pdf"
       system("pdftk #{filled_in_form_path} stamp /tmp/sig_pdf_#{unique_key}.pdf output #{stamped_app_without_cover_letter_path}")
       path_for_app_without_info_release_form = "/tmp/final_application_without_info_release_#{unique_key}.pdf"
-      cover_letter_path = File.expand_path("../calfresh/clean_cover_letter_v3.pdf", __FILE__)
+      cover_letter_path = File.expand_path("../calfresh/clean_cover_letter_v4.pdf", __FILE__)
       system("pdftk #{cover_letter_path} #{stamped_app_without_cover_letter_path} cat output #{path_for_app_without_info_release_form}")
       path_for_info_release_form = "/tmp/info_release_form_#{unique_key}.pdf"
       info_release_form = InfoReleaseForm.new(client_information: validated_field_input, signature_png_path: "/tmp/signature_#{unique_key}.png", path_for_pdf: path_for_info_release_form)
