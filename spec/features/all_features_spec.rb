743db84a2f58c945ac97977720f58a378934cfc2
Dave Guarino <dave@codeforamerica.org>
2015-03-03 17:34:27 -0800

    Fix JS validation of dates and improve error message

--- spec/features/all_features_spec.rb
+++ spec/features/all_features_spec.rb
@@ -19,6 +19,44 @@ feature 'User leaves required fields empty', :js => true do
   end
 end
 
+feature 'User puts in improperly-formatted date of birth', :js => true do
+  scenario 'bad date of birth for primary applicant' do
+    bad_date_of_birth = '01021985'
+    good_date_of_birth = '01/02/85'
+
+    visit '/application/basic_info?'
+    fill_in 'name', with: 'Hot Snakes'
+    fill_in 'home_address', with: "2015 Market Street"
+    fill_in 'home_zip_code', with: "94122"
+    click_button 'Next Step'
+    fill_in 'home_phone_number', with: "5555555555"
+    fill_in 'email', with: "hotsnakes@gmail.com"
+    click_on('Next Step')
+    fill_in 'date_of_birth', with: bad_date_of_birth
+    fill_in 'ssn', with: "000000000"
+    click_on('Next Step')
+    expect(page).to have_content('Please provide your date of birth in the format 02/18/85')
+    expect(page.current_path).to eq('/application/sex_and_ssn')
+
+    fill_in 'date_of_birth', with: good_date_of_birth
+    click_on('Next Step')
+
+    expect(page.current_path).to eq('/application/household_question')
+    click_link('Yes')
+    fill_in 'their_name', with: 'Hot Snakes'
+    fill_in 'their_date_of_birth', with: bad_date_of_birth
+    fill_in 'their_ssn', with: "000000000"
+    click_on('Next Step')
+
+    expect(page).to have_content('Please provide your date of birth in the format 02/18/85')
+
+    fill_in 'their_date_of_birth', with: good_date_of_birth
+    click_on('Next Step')
+
+    expect(page.current_path).to eq('/application/additional_household_question')
+  end
+end
+
 feature 'User goes through full application (up to review and submit)' do
   scenario 'with basic interactions' do
     visit '/application/basic_info?'
