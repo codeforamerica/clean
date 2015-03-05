1935c470157072994b6e0718373504cc83a612dd
Dave Guarino <dave@codeforamerica.org>
2015-03-05 09:51:19 -0800

    Config app to use Pacific time zone

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,7 @@ module CalfreshAndSoClean
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths << Rails.root.join('lib')
+
+    config.time_zone = "Pacific Time (US & Canada)"
   end
 end
